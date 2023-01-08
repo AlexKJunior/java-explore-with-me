759a3d11bb4288d2237ab580120f38936ccc0a72
AlexKJunior <poiuyytp@yandex.ru>
2023-01-09 01:12:44 +0400

    review 1

--- ewm-service/src/main/java/ru/practicum/ewm/compilation/model/Compilation.java
+++ ewm-service/src/main/java/ru/practicum/ewm/compilation/model/Compilation.java
@@ -4,7 +4,6 @@ import lombok.*;
 import ru.practicum.ewm.event.model.Event;
 
 import javax.persistence.*;
-import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 import java.util.Set;
 
